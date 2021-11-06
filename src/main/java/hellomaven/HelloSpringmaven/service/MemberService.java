386cb8df2bcd92694f9c72c31040623380f90a73
DrInnovatio <turing1723@gmail.com>
2021-11-06 16:19:12 +0900

    get MemberServiceTest done.
    Signed-off-by: DrInnovatio <turing1723@gmail.com>

--- src/main/java/hellomaven/HelloSpringmaven/service/MemberService.java
+++ src/main/java/hellomaven/HelloSpringmaven/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // Joining the membership.
     public Long join(Member member){
@@ -22,7 +26,7 @@ public class MemberService {
     private void validateDuplicatedMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalStateException("The name has existed already ");
+                    throw new IllegalStateException("The name has existed already.");
                 });
     }
 
--- src/test/java/hellomaven/HelloSpringmaven/service/MemberServiceTest.java
+++ src/test/java/hellomaven/HelloSpringmaven/service/MemberServiceTest.java
@@ -1,17 +1,68 @@
 package hellomaven.HelloSpringmaven.service;
 
+import hellomaven.HelloSpringmaven.domain.Member;
+import hellomaven.HelloSpringmaven.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class MemberServiceTest {
 
-    @Test
-    void join() {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
     }
 
     @Test
-    void findMembers() {
+    void join() {
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+
+        //When
+        Long saveId = memberService.join(member);
+
+        //Then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void duplicateMembers() {
+        // given
+
+        Member member1 = new Member();
+        member1.setName("James");
+
+        Member member2 = new Member();
+        member2.setName("James");
+
+        // when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+     /*   try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("The name has existed already.");
+        }*/
+
+        // then
+
     }
 
     @Test
