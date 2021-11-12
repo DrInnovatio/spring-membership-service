1125c7612399fb780b718f4056b9cb373ddbde7d
DrInnovatio <turing1723@gmail.com>
2021-11-12 20:21:10 +0900

    added "public String list" method.
    Signed-off-by: DrInnovatio <turing1723@gmail.com>

--- src/main/java/hellomaven/HelloSpringmaven/controller/MemberController.java
+++ src/main/java/hellomaven/HelloSpringmaven/controller/MemberController.java
@@ -4,8 +4,11 @@ import hellomaven.HelloSpringmaven.domain.Member;
 import hellomaven.HelloSpringmaven.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -27,11 +30,18 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
-        System.out.println("member = " + member);
+        System.out.println("member = " + member.getName());
 
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
